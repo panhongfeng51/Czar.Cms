e8e3ac7dc12b6f5c316036520ac639e01dc0194c
panhongfeng51 <754998852@qq.com>
2018-11-26 14:32:41 +0800

    新增.NET Core源代码解析配置文件及依赖注入

---
+++ Sample01/Sample01/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    /// <summary>
+    /// Content 控制器
+    /// </summary>
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+
+        public ContentController(IOptions<Content> option) {
+            contents = option.Value;
+        }
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            ContentViewModel con = new ContentViewModel();
+            var content = new List<Content>();
+            content.Add(contents);
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            con.Contents = content;
+            return View(con);
+        }
+    }
+}
---
+++ Sample01/Sample01/Sample01/Models/ContentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// Content 视图模式
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
